b086548ed6bf7de561921b0e8b0a9a69548a49e2
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-09 10:53:03 +0100

    classes

--- src/app/classes/teste.ts
+++ src/app/classes/teste.ts
@@ -1,2 +1,8 @@
 export class Teste {
+    id:string;
+    titre:string;
+    note:number;
+    sujet:string;
+    duree:string;
+    description:string;
 }
--- src/app/classes/utilisateur.ts
+++ src/app/classes/utilisateur.ts
@@ -1,2 +1,13 @@
 export class Utilisateur {
+    id:string;
+    nom:string;
+    age:number;
+    email:string;
+    mdp:string;
+    telephone:string;
+    adresse:string;
+    niveau:string;
+    specialite:string;
+    grade:string;
+    curriculum:string;
 }
