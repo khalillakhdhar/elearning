eda2f9a01d1e8676254eb66e15fe997dce04b3d0
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-27 10:56:48 +0100

    layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  {path:"",component: ConnexionComponent}
+  {path:"",component: ConnexionComponent},
+  {path:"home",component:LayoutComponent}
 ];
 
 @NgModule({
