02bbdf4292a0107a58c8becdf28f58418547c977
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-08 10:14:44 +0100

    Update utilisateur.component.ts

--- src/app/utilisateur/utilisateur.component.ts
+++ src/app/utilisateur/utilisateur.component.ts
@@ -1,4 +1,6 @@
+import { UserService } from './../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { Utilisateur } from '../classes/utilisateur';
 
 @Component({
   selector: 'app-utilisateur',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./utilisateur.component.css']
 })
 export class UtilisateurComponent implements OnInit {
-
-  constructor() { }
+user:Utilisateur;
+users:Utilisateur[];
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
+    this.user=new Utilisateur();
+    this.read();
   }
+ajouter()
+{
+  let us=Object.assign({},this.user);
+  this.userService.create_NewUser(us);
+}
+read()
+{
+this.userService.read_Users().subscribe(data => {
 
+  this.users = data.map(e => {
+    return {
+     id: e.payload.doc.id,
+
+     nom: e.payload.doc.data()["nom"],
+     age: e.payload.doc.data()["age"],
+     adresse: e.payload.doc.data()["adresse"],
+     niveau: e.payload.doc.data()["niveau"],
+     specialite: e.payload.doc.data()["specialite"],
+     email: e.payload.doc.data()["email"],
+     mdp: e.payload.doc.data()["mdp"],
+     telephone: e.payload.doc.data()["telephone"],
+     grade: e.payload.doc.data()["grade"],
+     curriculum_vitae: e.payload.doc.data()["curriculum_vitae"],
+
+
+
+    };
+  });
+
+
+  console.log("liste",this.users);
+
+});
+
+
+
+}
+supprimer(id)
+{
+  if(confirm("vous voulez supprimer l'utilisateur?"))
+  this.userService.delete_User(id);
+}
 }
