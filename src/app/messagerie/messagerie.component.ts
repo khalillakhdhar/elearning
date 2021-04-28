028feba56ed923510b7a46e18bf5638901e69d0c
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-28 14:26:42 +0100

    Update messagerie.component.ts

--- src/app/messagerie/messagerie.component.ts
+++ src/app/messagerie/messagerie.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Topic } from '../classes/topic';
+import { TopicService } from '../services/topic.service';
 
 @Component({
   selector: 'app-messagerie',
@@ -6,10 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messagerie.component.css']
 })
 export class MessagerieComponent implements OnInit {
+  topics: any;
+  topic: Topic;
+  constructor(private topicService: TopicService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.topic = new Topic();
+    this.read()
   }
+  CreateRecord() {
+    const record = {};
+    record['titre'] = this.topic.titre;
+    record['description'] = this.topic.description;
+    record['cle'] = this.topic.cle;
 
+    this.topicService.create_NewTopic(record).then(resp => {
+    console.log(resp);
+    alert('ajouté avec succés!');
+
+       })
+         .catch(error => {
+           console.log(error);
+         });
+       }
+       read()
+       {
+        this.topicService.read_Topics().subscribe(data => {
+
+          this.topics = data.map(e => {
+            return {
+             id: e.payload.doc.id,
+    
+             titre: e.payload.doc.data()["titre"],
+             description: e.payload.doc.data()["description"],
+             cle: e.payload.doc.data()["cle"],
+    
+    
+    
+            };
+          });
+          console.log(this.topics);
+    
+        });
+       }
+       access(key: string) {
+        localStorage.setItem('room', key);
+        //window.location.replace('chattopic');
+      }
 }
