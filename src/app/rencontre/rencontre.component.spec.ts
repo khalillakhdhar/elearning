33fa0d0c60ee7e1f09831fcc241c808707bf0177
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-05 09:40:34 +0100

    Create rencontre.component.spec.ts

---
+++ src/app/rencontre/rencontre.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RencontreComponent } from './rencontre.component';
+
+describe('RencontreComponent', () => {
+  let component: RencontreComponent;
+  let fixture: ComponentFixture<RencontreComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RencontreComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RencontreComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
